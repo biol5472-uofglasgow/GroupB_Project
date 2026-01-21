b5e0995252b384a93737237217e6e93df7a3b4a5
BeyondOurMinds <84035484+BeyondOurMinds@users.noreply.github.com>
2026-01-21 11:46:30 +0000

    Added __init__.py file and removed testing comments and function to clean up for actual project start

--- GroupB_Project5.py
+++ GroupB_Project5.py
@@ -1,8 +1,1 @@
-#### Wowie look at this! It's a project file whoah! #####
-#wow cool man!
-print("Hello from Group B Project 5!")
-
-# testing for dom
-
-def function test_DT():
-    print('this is a test for dom')
+# Project 5
