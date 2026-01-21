5310e8b876bee7001f4bd968f2bde7804a180fca
BeyondOurMinds <84035484+BeyondOurMinds@users.noreply.github.com>
2026-01-21 14:50:45 +0000

    Fixed pyproject.toml to use setuptools instead of uv, removed --help command in argparse to fix conflicts

--- src/Gene_Model_Summariser/cli.py
+++ src/Gene_Model_Summariser/cli.py
@@ -3,7 +3,6 @@ import argparse
 
 def app():
     parser = argparse.ArgumentParser(description="A simple CLI tool.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--help', action='help', help='Show this help message and exit.')
     print('hello world')
     
     return parser.parse_args()
