fbd828737948476c100ad6a0565a7b716e4ab591
BeyondOurMinds <84035484+BeyondOurMinds@users.noreply.github.com>
2026-01-21 14:40:43 +0000

    pushing to remove bad boy files

--- src/cli.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description="A simple CLI tool.")
-parser.add_argument('--help', action='help', help='Show this help message and exit.')
-
-def app():
-    print('hello world')
-    # Application logic goes here
