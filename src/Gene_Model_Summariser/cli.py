40c5153cc25736b43a618f4851ced663b4616c17
BeyondOurMinds <84035484+BeyondOurMinds@users.noreply.github.com>
2026-01-21 18:59:31 +0000

    added --gff and --fasta parser arguments

--- src/Gene_Model_Summariser/cli.py
+++ src/Gene_Model_Summariser/cli.py
@@ -3,6 +3,8 @@ import argparse
 
 def app():
     parser = argparse.ArgumentParser(description="A simple CLI tool.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-g','--gff', required=True, help='Path to GFF file')
+    parser.add_argument('-f','--fasta', help='Path to optional reference FASTA file for sequence-derived metrics')
     args = parser.parse_args()
     
     print('hello world')
